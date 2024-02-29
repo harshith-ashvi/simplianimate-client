51a7563fe530eceb85a1f55a6702d6809c92a3cc
harshith <harshith.ashvi@gmail.com>
2024-02-29 21:36:45 +0530

    install dependencies and structure files for  shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1>SimpliAnimate</h1>
+      <Button variant="destructive">Namaskara</Button>
     </>
   );
 }
