5b576b8c761c3501f33c2dca5dfacb3bd6920dce
harshith <harshith.ashvi@gmail.com>
2024-03-04 19:43:44 +0530

    add animation template screen nav bar component, toggle, toggle group ui components and lucid icons

---
+++ src/components/template-navbar.tsx
@@ -0,0 +1,62 @@
+import {
+  RectangleVertical,
+  RectangleHorizontal,
+  Square,
+  ChevronLeft,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+
+const TemplateNavbar = ({
+  screenResolution,
+  handlePressBack,
+  handleFormDataChange,
+  handleExportAnimation,
+}: {
+  screenResolution: string;
+  handlePressBack: () => void;
+  handleFormDataChange: (key: string, value: string) => void;
+  handleExportAnimation: () => void;
+}) => {
+  return (
+    <div
+      style={{
+        height: 45,
+        backgroundColor: "#f4f6fb",
+      }}
+      className="px-1 flex items-center justify-between"
+    >
+      <Button variant="ghost" onClick={handlePressBack}>
+        <ChevronLeft className="mr-2 h-5 w-5" /> Back
+      </Button>
+      <div className="flex items-center">
+        <ToggleGroup
+          type="single"
+          variant="outline"
+          defaultValue="Portrait"
+          value={screenResolution}
+          onValueChange={(value) => {
+            if (value) {
+              handleFormDataChange("screenResolution", value);
+            }
+          }}
+        >
+          <ToggleGroupItem value="Portrait" aria-label="Toggle Portrait">
+            <RectangleVertical className="h-6 w-6" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="Landscape" aria-label="Toggle Landscape">
+            <RectangleHorizontal className="h-6 w-6" />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="Square" aria-label="Toggle Square">
+            <Square className="h-6 w-6" />
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+      <Button size="sm" onClick={handleExportAnimation}>
+        Export
+      </Button>
+    </div>
+  );
+};
+
+export default TemplateNavbar;
