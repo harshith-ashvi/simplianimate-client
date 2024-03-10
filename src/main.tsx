b23fb4d5937647f56d8b4b2e16fa9d5b07251ba2
harshith <harshith.ashvi@gmail.com>
2024-03-10 15:03:40 +0530

    add posthog for analytics and .env and update .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import posthog from "posthog-js";
 import App from "./App.tsx";
 import "./index.css";
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
