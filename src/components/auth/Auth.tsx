1e535cebac5531f76e93172aa511033abcb7f502
Harshith Ashvi <harshith.ashvi@gmail.com>
2024-07-07 16:48:35 +0530

    add auth context

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,111 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { User } from "@supabase/supabase-js";
+
+import supabase from "@/data/supabaseClient";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
+
+const AuthContext = createContext<{
+  user: User | null;
+  signin: ({ email, password }: FormValues) => void;
+  signinWithProvider: () => void;
+  signout: () => void;
+}>({
+  user: null,
+  signin: () => {},
+  signinWithProvider: () => {},
+  signout: () => {},
+});
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const setData = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+      if (error) throw error;
+      setUser(session?.user ?? null);
+      setLoading(false);
+    };
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+
+    setData();
+
+    return () => listener?.subscription.unsubscribe();
+  }, []);
+
+  const signinWithProvider = async () => {
+    setLoading(true);
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+      });
+      if (error) throw error;
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signin = async ({ email, password }: FormValues) => {
+    setLoading(true);
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) throw error;
+      navigate("/");
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signout = async () => {
+    setLoading(true);
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) throw error;
+      navigate("/signin");
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    user,
+    signin,
+    signinWithProvider,
+    signout,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+// export the useAuth hook
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
